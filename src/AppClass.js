7b27e18006eb75431c1e19836558025ff6767f9c
Akanksha Agrawal <akanksha.agrawal@kalvium.community>
2022-12-21 20:52:01 +0530

    initial commit

--- src/AppClass.js
+++ src/AppClass.js
@@ -1,49 +1,17 @@
 import { Component } from "react";
 import "./App.css"
-import elephant from "./images/elephant.jpeg";
 
 export default class AppClass extends Component{
-  
-  imageData = ()=>{
-    let data = [
-      {
-        id:1,
-        img:elephant
-      },
-      {
-        id:2,
-        img:elephant
-      },
-      {
-        id:3,
-        img:elephant
-      },
-      {
-        id:4,
-        img:elephant
-      }
-    ]
-    return data;
-  }
 
-// code here
   render(){
-    const imgData = this.imageData();
-    return (
-      <div>
-        <h1>Kalvium gallary</h1>
-        <div className="row">
-        {
-         imgData.map((image) => {
-          return(
-            <div className="column"key={image.id}>
-              <img src={image.img} alt=""/>
-            </div>
-          )
-          
-          })};
-      </div>
-  
+    return(
+      <div className='App'>
+        <h1>Kalvium Gallery</h1>
+        <div className='Gallery'>
+          {this.props.imageData.map((e) => {
+           return (<img src={e.img} alt='img(i)' />)
+        })}
+        </div>
       </div>
     );
   }
